6beb992672599ec6dec35749d704c58f85990c02
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2024-03-18 13:38:45 -0300

    Create coinsProb.r

---
+++ coinsProb.r
@@ -0,0 +1,36 @@
+#Exercicio de probabilidade
+#Determinar para o lancamento de 4 moedas
+
+# A - Qual o espaco amostral
+# B - Probabilidade de obter um numero par de Caras
+# C - Probabilidade de obter 3 Caras
+# D - Probabilidade de obter ao menos 2 Caras consecutivas
+# E - Probabilidade de obter 3 Caras ou um numero par de Caras
+# F - Probabilidade de obter exatamente 3 Caras e ao menos 2 Caras consecutivas
+
+#Resolucao A:
+# Uma moeda possui cara e coroa
+# Sao lancadas 4^2 = 16
+
+#Resolucao B:
+# 8/16 = 0,5
+
+#Resolucao C:
+# 4/16 = 0,25
+
+#Resolucao D:
+#nemunha cara consecutiva  > 2 casos
+#apenas 1 cara consecutiva > 4 casos
+# 16 - 6 = 10
+# 10/16 = 0,625
+
+#Resolucao E:
+# P(C) + P(B) - P(C) = 0,25 + 0,5 - 0,25 = 0,5
+
+#Resolucao F:
+#K =  coroa , C = Cara
+# (CCCK, KCCC, CCKC e CKCC)
+# 4/16 = 0,25
+
+
+
